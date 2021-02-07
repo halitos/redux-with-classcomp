fb746a51f2086a2af636ad8bd759ddffde9853bd
halit <oskan96@gmail.com>
2021-02-07 17:20:00 +0000

    redux first connecttion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
